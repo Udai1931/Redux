1b1d276682586fc9c1644020d0d7393580ef0569
Udai Gupta <udaigupta19311@gmail.com>
2021-09-08 16:03:06 +0530

    resume builder redux done

--- ui/src/redux/reducers/rootReducer.js
+++ ui/src/redux/reducers/rootReducer.js
@@ -2,10 +2,12 @@ import {combineReducers} from 'redux';
 import documentReducer from './documentReducer';
 import contactReducer from './contactReducer';
 import educationReducer from './educationReducer';
+import authReducer from './authReducer';
 import {firestoreReducer} from 'redux-firestore';
 import {firebaseReducer} from 'react-redux-firebase';
 
 const rootReducer = combineReducers({
+    auth:authReducer,
     firestore:firestoreReducer,
     firebase:firebaseReducer,
     document:documentReducer,
